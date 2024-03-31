0248f55e836971e9b57dc2410965ab42d0db916b
dimarick <dimon.ksk@gmail.com>
2024-03-31 16:59:13 +0500

    Оптимизация кодека видео

--- launch.sh
+++ launch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-./embedded_cv $1 | ffmpeg -f rawvideo -pixel_format bgr24 -video_size 640x480 -re  -i - -f h264 -preset ultrafast -tune zerolatency  -framerate 15 -fflags nobuffer -flags low_delay -avioflags direct -fflags discardcorrupt -g 10 $2
+./embedded_cv $1 | ffmpeg -f rawvideo -pixel_format bgr24 -s 640x480 -re  -i - \
+  -f mpegts -level:v high -profile:v high -b:v 4000k -framerate 15 -fflags nobuffer -flags low_delay -avioflags direct -fflags discardcorrupt -g 20 $2
