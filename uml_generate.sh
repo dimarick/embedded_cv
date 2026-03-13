ee31c6beb9a54ae14797eab3f2503da4f4728441
dimarick <dimon.ksk@gmail.com>
2026-03-14 02:25:43 +0500

    Подправил диаграмму

--- uml_generate.sh
+++ uml_generate.sh
@@ -12,7 +12,7 @@ cat /tmp/classes.puml | grep -Ev '^\s+-' | grep -Ev '_DIM_' | grep -Eo '^\s*.{0,
 
 FILES=`ls src/mini_server/**/*.h | xargs -n 1 -I{} echo -i {}`
 hpp2plantuml $FILES -d -o /tmp/classes.puml
-cat /tmp/classes.puml | grep -Ev '^\s+-' | sed -e '/^@startuml$/a\left to right direction' > classes_mini_server.puml
+cat /tmp/classes.puml | grep -Ev '^\s+-' | grep -Eo '(^\s+.{0,80})|(\S.*)' | sed -e '/^@startuml$/a\left to right direction' > classes_mini_server.puml
 
 FILES=`ls ws_ctl/**/*.h | grep -Ev '.*build.*' | grep -Ev '.*web.*' | grep -Ev '.*3rdparty.*' | grep -Ev '.*release.*' | xargs -n 1 -I{} echo -i {}`
 hpp2plantuml $FILES -d -o /tmp/classes.puml
