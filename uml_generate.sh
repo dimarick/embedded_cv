e96cb4f7417f5176458a3c49ffbd57a4e722e3d1
dimarick <dimon.ksk@gmail.com>
2026-03-13 16:09:31 +0500

    Генерация диаграмм классов

---
+++ uml_generate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Включаем поддержку **
+shopt -s globstar nullglob
+
+FILES=`ls src/**/*.h | grep -Ev '.*calibrator.*' | grep -Ev '.*mini_server.*' | xargs -n 1 -I{} echo -i {}`
+hpp2plantuml $FILES -d -o /tmp/classes.puml
+cat /tmp/classes.puml | grep -Ev '^\s+-' | grep -Ev '_DIM_' | grep -Eo '^\s*.{0,80}' | sed -e '/^@startuml$/a\left to right direction' > classes_main.puml
+
+FILES=`ls src/calibrator/**/*.h | xargs -n 1 -I{} echo -i {}`
+hpp2plantuml $FILES -d -o /tmp/classes.puml
+cat /tmp/classes.puml | grep -Ev '^\s+-' | grep -Ev '_DIM_' | grep -Eo '^\s*.{0,80}' > classes_calibrator.puml
+
+FILES=`ls src/mini_server/**/*.h | xargs -n 1 -I{} echo -i {}`
+hpp2plantuml $FILES -d -o /tmp/classes.puml
+cat /tmp/classes.puml | grep -Ev '^\s+-' | sed -e '/^@startuml$/a\left to right direction' > classes_mini_server.puml
+
+FILES=`ls ws_ctl/**/*.h | grep -Ev '.*build.*' | grep -Ev '.*web.*' | grep -Ev '.*3rdparty.*' | grep -Ev '.*release.*' | xargs -n 1 -I{} echo -i {}`
+hpp2plantuml $FILES -d -o /tmp/classes.puml
+cat /tmp/classes.puml | grep -Ev '^\s+-' > classes_ws_ctl.puml
+
+plantuml -tsvg classes_*.puml
